42b7497528652e26260a8097c45b7a4778201b19
Justin M <manningjustin08@yahoo.com>
2023-08-01 12:15:37 -0400

    Initial commit, I created repository in ghub but didnt commit assignment

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Header from "./Header";
+import { Container } from "./Container.styled";
+
+function App() {
+  return (
+    <Container>
+      <h1>Hello World</h1>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/Container.styled.js
@@ -0,0 +1,9 @@
+import { Styled } from "styled-components/dist/constructors/constructWithOptions";
+
+export const Container = styled.div`
+width:1000px;
+max-width:100%;
+padding: 0 20px;
+margin: 0 auto;
+`
+
